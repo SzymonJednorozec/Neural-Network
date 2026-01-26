5ce3ba905c163f7a7993f602c235ba0056b32960
Jakub <jakub.konecki9@gmail.com>
2026-01-26 22:54:05 +0100

    feat: add categorical cross entropy | feat: add calculations for derivatives in relu

--- app.py
+++ app.py
@@ -8,11 +8,23 @@ class Layer_Relu():
         self.output = np.zeros(output)
         self.biases = np.zeros(output)
         self.weights = np.random.uniform(-1,1,size=(input_size,output))
+
+        self.d_biases 
+        self.d_weights
+        self.d_inputs
            
     def feedForward(self,input_vals): 
         self.input_vals = np.atleast_2d(input_vals)
         self.output = Relu(np.dot(self.input_vals,self.weights) + self.biases) #Relu(dot product + biases)       
         return self.output
+    
+    def backPropagate(self,dvalues): #dvalues - accumulated derivatives to this point
+        dvalues = dvalues.copy()
+        dvalues = np.where(self.output > 0, dvalues, 0)
+        self.d_biases = np.sum(dvalues,axis=0,keepdims=True)
+        self.d_weights = np.dot(self.input_vals.T,dvalues)
+        self.d_inputs = np.dot(dvalues,self.weights.T)
+        return self.d_inputs
     
 class Layer_softmax(Layer_Relu):
     def feedForward(self,input_vals):
@@ -40,9 +52,19 @@ def Relu(arr):
 
 def softmax(arr):
     exponential_vals = np.exp(arr)
-    outcome = exponential_vals/np.sum(exponential_vals,axis=1)
+    outcome = exponential_vals/np.sum(exponential_vals,axis=1,keepdims=True)
     return outcome
+
+def cross_entropy(arr,target):
+    if np.ndim(target) == 2:
+        outcome = -np.sum(target*np.log(arr),axis=1)
+    elif np.ndim(target) == 1:
+        batch_size = arr.shape[0]
+        outcome =  -np.log(arr[np.arange(batch_size), target])
+    
+    return outcome
+
 
 if __name__=='__main__':
     network = Neural_network([4,7,2])
-    print(network.feedForward([1,1,1,1]))
+    print(cross_entropy(network.feedForward([1,1,1,1]),[1]))
