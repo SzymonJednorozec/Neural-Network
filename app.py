7b21feff0cc0d37596b593a0b6f4731f032205a6
Jakub <jakub.konecki9@gmail.com>
2026-02-08 19:59:24 +0100

    feat: add adam optimizer

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class Neural_network():
 class Optimizer():
     def __init__(self,alfa,decay=0.001):
         self.learning_rate=alfa
-        self.current_lr
+        self.current_lr=alfa
         self.decay=decay
         self.iteratrions=0 
     
@@ -82,6 +82,54 @@ class Optimizer():
 
 
         self.iteratrions+=1
+
+class Adam_optimizer():
+    def __init__(self,learning_rate=1.,decay=0.,epsilon=1e-7,beta1=0.9,beta2=0.99,w_lambda2=0,b_lambda2=0):
+        self.learning_rate=learning_rate
+        self.decay=decay
+        self.current_learning_rate=learning_rate
+        self.iterations=0
+        self.epsilon = epsilon
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.w_lambda2 = w_lambda2
+        self.b_lambda2 = b_lambda2
+
+    def optimize(self ,neural_network):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * 1./(1.+self.decay*self.iterations)
+
+        for layer in neural_network.layers:
+            if not hasattr(layer,'weight_momentum'):
+                layer.weight_momentum = np.zeros_like(layer.weights)
+                layer.bias_momentum = np.zeros_like(layer.biases)
+                layer.weight_cache = np.zeros_like(layer.weights)
+                layer.bias_cache = np.zeros_like(layer.biases)
+
+
+            layer.weight_momentum = (self.beta1 * layer.weight_momentum + (1 - self.beta1) * layer.d_weights)
+            layer.bias_momentum = (self.beta1 * layer.bias_momentum + (1 - self.beta1) * layer.d_biases)
+
+            t=self.iterations+1
+            w_momentum_term = (layer.weight_momentum) / (1-self.beta1**t)
+            b_momentum_term = (layer.bias_momentum) / (1-self.beta1**t)
+
+
+            layer.weight_cache = (self.beta2 * layer.weight_cache + (1-self.beta2) * np.square(layer.d_weights))
+            layer.bias_cache = (self.beta2 * layer.bias_cache + (1-self.beta2) * np.square(layer.d_biases))
+
+            w_cache_term = (layer.weight_cache) / (1-self.beta2**t)
+            b_cache_term = (layer.bias_cache) / (1-self.beta2**t)
+
+
+            w_alfa_factor = w_momentum_term/(np.sqrt(w_cache_term) + self.epsilon)
+            b_alfa_factor = b_momentum_term/(np.sqrt(b_cache_term) + self.epsilon)
+            # --------------------------
+
+            layer.weights += -(self.current_learning_rate * w_alfa_factor)
+            layer.biases += -(self.current_learning_rate * b_alfa_factor)   
+        
+        self.iterations += 1
 
 
 
